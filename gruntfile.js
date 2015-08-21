42b839e191cbcbd3f61288a5748daf01ca908450
Nicholas Thomas <nrt012@amfam.com>
2015-08-21 11:38:47 -0500

    Add grunt tasks, add bower stub
    Added live reload, watch and sass processing to grunt,
    Added bower stub.

--- gruntfile.js
+++ gruntfile.js
@@ -11,12 +11,61 @@ module.exports = function(grunt) {
 				src : ['components/scripts/*.js'],
 				dest : 'builds/development/js/script.js'
 			}
+		}, //concat
+
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: [{
+					src: 'components/sass/style.scss',
+					dest: 'builds/development/css/style.css'
+				}]
+			}
+		}, //sass
+
+		watch: {
+			options: {
+  				spawn: false,
+  				livereload: true
+			},
+  			scripts: {
+    			files: ['builds/development/**/*.html',
+    				'components/scripts/**/*.js', 
+    				'components/sass/**/*.scss'
+    			],
+    			tasks: ['concat', 'sass'],
+  			},
+		}, 
+
+		connect: {
+			server: {
+				options: {
+					hostname: 'localhost',
+					port: 3000,
+					base: 'builds/development/',
+					livereload: true
+				}
+			}
 		}
+
 
 	}); //Init Config
 
+	// Concat javascript files
 	grunt.loadNpmTasks('grunt-contrib-concat');
 
-	grunt.registerTask('default', ['concat']);
+	// Compile sass files into css
+	grunt.loadNpmTasks('grunt-sass');
+
+	//Watch for changes to html, js and scss files
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	//Live reload middleware
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	//main grunt
+	grunt.registerTask('default', ['concat', 'sass', 'connect', 'watch']);
 
 }; //Wrapper function
